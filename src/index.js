4d5857cacba0a0e7e0311c7f7d653b9c01aeba5a
Liam <liam99mail@proton.me>
2025-06-12 15:38:35 +0200

    Fetch and extract data for a given location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+import getWeatherData from "./collection";
+import extractData from "./processing";
+
+getWeatherData('Wageningen')
+.then(rawData => extractData(rawData))
+.then(data => console.log(data))
+
+
